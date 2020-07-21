78ed72480ab8871d12613e0b6d7402af27a0cb88
HamineOliveira <hamine.oliveira@unesp.br>
2020-07-20 22:09:58 -0300

    Hamine Oliveira

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##Armazenando o inverso de uma matriz
+#criar a matriz especial
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y  ##<<- atribui valor
+    i <<- NULL
+  }
+  get <- function() x  ##vai criar a inversa
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+##Esta função calcula o inverso da "matriz" especial retornada makeCacheMatrixacima. 
+  ##Se o inverso já tiver sido calculado (e a matriz não tiver sido alterada), 
+  ##você cacheSolvedeverá recuperar o inverso do cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message("obtendo dados")  ##se já existe retorna mensagem
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
